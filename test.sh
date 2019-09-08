a0bd6faac9c559e08b58c090633edb0396de314b
chiayuhsu <chiayuhsu@asu.edu>
2019-09-08 18:03:56 +0000

    change test script

--- test.sh
+++ test.sh
@@ -1,13 +1,4 @@
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_1.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_2.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_3.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_4.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_5.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_6.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_7.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_8.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_9.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_10.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_11.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_12.pth --out result.pkl --eval bbox 2>&1 | tee -a ./test.log
-
+for iter in {91..141}
+do
+	python -u tools/test.py configs/faster_rcnn_r50_fpn_1x.py work_dirs/faster_rcnn_r50_fpn_1x/epoch_$iter.pth --out result.pkl --eval bbox 2>&1 | tee -a ./train3.log
+done
